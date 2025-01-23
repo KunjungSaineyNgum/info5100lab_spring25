0346810af3941c69086fc6429a664f153d04e95e
kunjungsaineyngum <ngum.k@northeastern.edu>
2025-01-23 16:01:28 -0500

    added the java code

---
+++ Lab1_helloword/src/lab1_helloword/Lab1_helloword.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1_helloword;
+
+/**
+ *
+ * @author Student
+ */
+public class Lab1_helloword {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+}
+    }
+    
+  
